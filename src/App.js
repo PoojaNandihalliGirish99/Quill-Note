14b26d4c19aaada116dc9b729a28c8d43fc24f7c
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-04-28 11:28:03 +0530

    editor and sidebar components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase';
+import SidebarComponent from './sidebar/sidebar';
+import EditorComponent from './editor/editor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null,
+  };
+  }
+
+  render() {
+    return(
+      <div className="app-container">
+      <SidebarComponent 
+      selectedNoteIndex={this.state.selectedNoteIndex}
+      notes = {this.state.notes}
+      deleteNote = {this.deleteNote}
+      selectNote = {this.selectNote}
+      newNote = {this.newNote}>
+      </SidebarComponent>
+      {
+        this.state.selectedNote ? 
+        <EditorComponent selectedNote = {this.state.selectedNote}
+      selectedNoteIndex= {this.state.selectedNoteIndex}
+      notes = {this.state.notes}></EditorComponent> :
+      null
+      }
     </div>
-  );
+    );
+  }
+
+  //hook - component didMount: this gets called when app component is mounted
+  componentDidMount = () => {
+    firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+      const notes = serverUpdate.docs.map(_doc => {
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({notes: notes});
+    });
+  }
+
+  selectNote = (note, index) => this.setState({selectedNoteIndex: index, selectNote: note});
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKPJ5EgF4ETMKWY3GYRV6mQ0-Whlr0dW8",
+    authDomain: "quill-note-d6b6c.firebaseapp.com",
+    projectId: "quill-note-d6b6c",
+    storageBucket: "quill-note-d6b6c.appspot.com",
+    messagingSenderId: "1086774891838",
+    appId: "1:1086774891838:web:54704614f473012cf9486f",
+    measurementId: "G-5FL7H6LEGG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
